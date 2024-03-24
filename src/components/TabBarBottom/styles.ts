4c7bb4141be01be21ac05536204131d7f8e1a64f
Victor Hugo <vhgouvea@hotmail.com>
2024-03-24 17:23:00 -0300

    updated tabBarBottom

--- src/components/TabBarBottom/styles.ts
+++ src/components/TabBarBottom/styles.ts
@@ -6,14 +6,11 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: ${RFValue(10)}px;
 `;
 
 export const ItemTab = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
-  width: 95%;
-  height: 100%;
   border-top: 2px solid;
 `;
 
@@ -21,4 +18,10 @@ export const Icon = styled(FontAwesome)`
   font-size: 20px;
   color: ${({ theme }) => theme.colors.principal};
   margin-right: ${RFValue(10)}px;
+`;
+
+export const Text = styled.Text`
+  font-size: ${RFValue(12)}px;
+  line-height: ${RFValue(19.92)}px;
+  font-family: ${({ theme }) => theme.fonts.roboto_regular};
 `;
