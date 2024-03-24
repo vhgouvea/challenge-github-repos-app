e5846dc07418a618ed783d80d9e6ea8ac190602c
Victor Hugo <vhgouvea@hotmail.com>
2024-03-24 17:23:41 -0300

    add table and model to insert repos

---
+++ src/database/models/RepositoryModel.ts
@@ -0,0 +1,9 @@
+export interface RepositoryModel {
+  id: number;
+  full_name: string;
+  description: string;
+  avatar_url: string;
+  stargazers_count: number;
+  language: string;
+  html_url: string;
+}
---
+++ src/database/tables/TblRepository.tsx
@@ -0,0 +1,6 @@
+import { MMKV } from 'react-native-mmkv';
+
+
+export const TblRepository = new MMKV({
+  id: 'tblRepository'
+});
