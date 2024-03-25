a9c347f3b89d1598865b3a36afad6d54545a783f
Victor Hugo <vhgouvea@hotmail.com>
2024-03-25 00:08:21 -0300

    modify custom header component

--- src/components/CustomHeader/index.tsx
+++ src/components/CustomHeader/index.tsx
@@ -1,27 +1,92 @@
-import React from "react";
-import { HeaderContainer, HeaderText, RightComponentContainer } from "./styles";
-import { TouchableOpacity } from "react-native";
-import { FontAwesome6 } from "@expo/vector-icons";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { BottomSheetModal, BottomSheetScrollView, BottomSheetTextInput, BottomSheetView } from "@gorhom/bottom-sheet";
+
+import { 
+  Container, 
+  Content, 
+  ContentBottomSheet, 
+  ContentButtonBottomSheet, 
+  HeaderText, 
+  Icon, 
+  IconButton, 
+  Img,
+  TitleBottomSheet
+} from "./styles";
+
+import Gear from '../../assets/Vectorgear.png';
+import { CustomButton } from "../CustomButton";
+import { CustomInput } from "../CustomInput";
+import { AntDesign } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useRepositoryData } from "../../context/useRepositoryData";
 
 interface Props {
-
+  getRepos: (param: string) => void;
+  setParamGetRepos: (param: string) => void;
+  paramGetRepos: string;
 }
 
-export function CustomHeader() {
+export function CustomHeader({ getRepos, setParamGetRepos, paramGetRepos }: Props) {
 
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
-  function onClick() {
-    console.log('Cliquei na engrenagem')
-  }
+  // variables
+  const snapPoints = useMemo(() => ['38%'], []);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const updateRepository = useCallback(async() => {
+    try {
+
+      getRepos(paramGetRepos)
+      bottomSheetModalRef.current?.close()
+
+    } catch (error) {
+
+    }
+
+  }, [paramGetRepos])
+
 
   return (
-    <HeaderContainer>
-      <HeaderText>Teste</HeaderText>
-      <RightComponentContainer>
-        <TouchableOpacity onPress={onClick}>
-          <FontAwesome6 name="gear" size={24} />
-        </TouchableOpacity>
-      </RightComponentContainer>
-    </HeaderContainer>
+    <Container>
+
+      <Content>
+        <HeaderText>Wefit</HeaderText>
+      </Content>
+
+      <IconButton onPress={handlePresentModalPress}>
+        <Img source={Gear} resizeMode="contain"/>
+      </IconButton>
+
+
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+      >
+        <ContentBottomSheet>
+
+          <TitleBottomSheet>Alterar usuário selecionado</TitleBottomSheet>
+
+          <CustomInput 
+            onChangeText={setParamGetRepos}
+            value={paramGetRepos}
+            placeholder="Nome do usuário"
+            keyboardType="default"
+          />
+          <ContentButtonBottomSheet>
+            <CustomButton width={`${48.5}%`} loading={false} onPress={() => {bottomSheetModalRef.current?.close()}} title="Cancelar" invertColors={true}/>
+            <CustomButton width={`${48.5}%`} loading={false} onPress={updateRepository} title="Salvar" invertColors={false}/>
+          </ContentButtonBottomSheet>
+        </ContentBottomSheet>
+        
+      </BottomSheetModal>
+    </Container>
   );
 }
